fcc8bfd5ca94b1b61afff5b1db3d9c5f3be88918
MaxwellVolz <Maxwellvolz@gmail.com>
2014-06-12 12:56:54 -0700

    Create row generator
    startIt() was a long time for 2 lines hah

--- web/script.js
+++ web/script.js
@@ -6,8 +6,10 @@ $(document).ready(sizeContent);
 //Every resize of window
 $(window).resize(sizeContent);
 
+
 //Dynamically assign height
 function sizeContent() {
+	
 	
 	//determine sizes
 	var pageWidth = $("body").width();
@@ -15,5 +17,48 @@ function sizeContent() {
 	
 	$("#pageContainer").css("width",pageWidth*2);
 	
-	$(".page").css("width",pageWidth/2);
+	$(".page").css("width",pageWidth);
+}
+
+var playerCount = 0;
+
+
+$(document).ready(function() {
+	
+	
+	$( "div#plus" ).mousedown(function(){
+		$("div#startGame").css("opacity",1);
+		
+		++playerCount;
+		$( "h1#players" ).html( playerCount );
+	});
+	
+	$( "div#minus" ).mousedown(function(){
+		if(playerCount > 0){
+			--playerCount;
+		}
+		if(playerCount == 0){
+			$("div#startGame").css("opacity",0);
+		}
+		
+		$( "h1#players" ).html( playerCount );
+	});
+	
+	$( "div#startGame" ).mousedown(function(){
+		startIt();
+		$( "div#scoreboard" ).css("top","-100%");
+	});
+	
+});
+
+function startIt(){
+	while (playerCount > 0) { 
+		$("tr#mainRow td#mainD").append( 
+		
+			"<td><input type='text'/></td>"	
+			
+			);
+		
+		--playerCount;
+	}
 }
