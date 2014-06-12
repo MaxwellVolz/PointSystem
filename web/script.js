4772b544549abce10cebf44abcd59f029283ff13
MaxwellVolz <Maxwellvolz@gmail.com>
2014-06-11 21:46:18 -0700

    Create basic pages layout
    add title,table,cells to page1

--- web/script.js
+++ web/script.js
@@ -1,1 +1,21 @@
 // JavaScript Document
+
+//Initial load of page
+$(document).ready(sizeContent);
+
+//Every resize of window
+$(window).resize(sizeContent);
+
+//Dynamically assign height
+function sizeContent() {
+	
+	//determine sizes
+	var pageWidth = $("body").width();
+	var pageHeight = $("body").height();
+	
+	$("#pageContainer").css("width",pageWidth*5);
+	
+	$(".page").css("width",pageWidth/2);
+	
+	
+}
